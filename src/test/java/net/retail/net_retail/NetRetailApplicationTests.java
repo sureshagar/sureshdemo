baf581606ef90119c99f23bf45c683aee641d5ea
sureshagar <51161480+sureshagar@users.noreply.github.com>
2019-05-29 19:14:03 +0530

    test file

--- src/test/java/net/retail/net_retail/NetRetailApplicationTests.java
+++ src/test/java/net/retail/net_retail/NetRetailApplicationTests.java
@@ -9,8 +9,43 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class NetRetailApplicationTests {
 
+	@Autowired
+	private BillUtil billUtil;
 	@Test
-	public void contextLoads() {
+	public void getNetAmount() {
+
+		Customer c = new Customer();
+		Product p = new Product();
+		Product p1 = new Product();
+
+		Request rs = new Request();
+		List<Product> products= new ArrayList<Product>();
+
+		c.setCustomerName("suresh");
+		c.setIsEmployee(false);
+		c.setIsAffliate(true);
+		c.setCustomerSince(new Date());
+
+
+		p.setGroceries(true);
+		p.setProductName("atta");
+		p.setUnitPrice(1);
+		p.setUnits(20);
+
+
+		p1.setGroceries(true);
+		p1.setProductName("atttatat");
+		p1.setUnitPrice(1);
+		p1.setUnits(20);
+
+		products.add(p1);
+		products.add(p);
+		rs.setCustomer(c);
+		rs.setProducts(products);
+		Response testres = billUtil.getNetAmount(rs);
+
+		assert (testres.getBillTotal() == 40);
+
 	}
 
 }
